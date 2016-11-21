0524ee147b5d02d2ec9d944845d9faa688dc146c
Ted Zhu <tedz2usa@gmail.com>
2016-11-21 04:04:35 -0500

    More code.

--- main.js
+++ main.js
@@ -5,15 +5,37 @@ log("Hello!");
 
 window.onload = init;
 
-var submitButton;
+var submitButton, inputTextArea;
+var rawLines;
 
 function init() {
 
   submitButton = document.getElementById('submitButton');
   submitButton.onclick = submit;
 
+  inputTextArea = document.getElementById('inputText');
+
+
 }
 
 function submit() {
   log("Button Clicked!");
+
+  getRawLines();
+  getLeftIndent();
+
+
+}
+
+function getRawLines() {
+  rawLines = inputTextArea.value.split("\n");
+  log(rawLines);
+}
+
+function getLeftIndent() {
+
+  for (var i = 0; i < rawLines.length; i++) {
+    log(rawLines[i]);
+  }
+
 }
