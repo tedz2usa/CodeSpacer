dc673e18878a489344549d6a26e27fdfbca5f24c
Ted Zhu <tedz2usa@gmail.com>
2016-11-21 03:58:29 -0500

    Add listener for submit button.

--- main.js
+++ main.js
@@ -2,3 +2,18 @@
 var log = console.log.bind(console);
 
 log("Hello!");
+
+window.onload = init;
+
+var submitButton;
+
+function init() {
+
+  submitButton = document.getElementById('submitButton');
+  submitButton.onclick = submit;
+
+}
+
+function submit() {
+  log("Button Clicked!");
+}
